636995caba2beb5d0bdf0ecbbb857907d1dc279d
wangshuodong <wawngshuodong2004@163.com>
2018-03-28 15:25:04 +0800

    wangsd

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import Navi from './components/Navi/Navi.vue'
 
 Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Navi)
 })
